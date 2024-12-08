d4ebc8c28747f274bf089e86278b65cebdc3eb47
Afzal Khan <80944849+khan1020@users.noreply.github.com>
2024-12-08 21:54:29 +0500

    Add files via upload

---
+++ spotless hungry crocodile-vue/src/components/features1.vue
@@ -0,0 +1,263 @@
+<template>
+  <div class="thq-section-padding">
+    <div class="features1-container2 thq-section-max-width">
+      <div class="features1-image-container">
+        <img
+          :alt="feature1ImgAlt"
+          :src="feature1ImgSrc"
+          v-if="activeTab === 0"
+          class="features1-image1 thq-img-ratio-16-9"
+        />
+        <img
+          :alt="feature2ImgAlt"
+          :src="feature2ImgSrc"
+          v-if="activeTab === 1"
+          class="features1-image2 thq-img-ratio-16-9"
+        />
+        <img
+          :alt="feature3ImgAlt"
+          :src="feature3ImgSrc"
+          v-if="activeTab === 2"
+          class="features1-image3 thq-img-ratio-16-9"
+        />
+      </div>
+      <div class="features1-tabs-menu">
+        <div @click="activeTab = 0" class="features1-tab-horizontal1">
+          <div class="features1-divider-container1">
+            <div v-if="activeTab === 0" class="features1-container3"></div>
+          </div>
+          <div class="features1-content1">
+            <h2 class="thq-heading-2">{{ feature1Title }}</h2>
+            <span class="thq-body-small">{{ feature1Description }}</span>
+          </div>
+        </div>
+        <div @click="activeTab = 1" class="features1-tab-horizontal2">
+          <div class="features1-divider-container2">
+            <div v-if="activeTab === 1" class="features1-container4"></div>
+          </div>
+          <div class="features1-content2">
+            <h2 class="thq-heading-2">{{ feature2Title }}</h2>
+            <span class="thq-body-small">{{ feature2Description }}</span>
+          </div>
+        </div>
+        <div @click="activeTab = 2" class="features1-tab-horizontal3">
+          <div class="features1-divider-container3">
+            <div v-if="activeTab === 2" class="features1-container5"></div>
+          </div>
+          <div class="features1-content3">
+            <h2 class="thq-heading-2">{{ feature3Title }}</h2>
+            <span class="thq-body-small">{{ feature3Description }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Features1',
+  props: {
+    feature2Title: {
+      type: String,
+      default: 'Real-time Health Monitoring',
+    },
+    feature1ImgSrc: {
+      type: String,
+      default:
+        'https://images.unsplash.com/photo-1666887360862-75274811b27f?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTczMzUwMDE2M3w&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    feature1ImgAlt: {
+      type: String,
+      default: 'Smart Healthcare Portal Feature 1 Image',
+    },
+    feature3ImgSrc: {
+      type: String,
+      default:
+        'https://images.unsplash.com/photo-1559839734-2b71ea197ec2?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTczMzUwMDE2Mnw&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+    feature3Description: {
+      type: String,
+      default: 'Access to personalized health records and medical history',
+    },
+    feature2Description: {
+      type: String,
+      default: 'Real-time monitoring of vital signs and health metrics',
+    },
+    feature3Title: {
+      type: String,
+      default: 'Personalized Health Records',
+    },
+    feature3ImgAlt: {
+      type: String,
+      default: 'Smart Healthcare Portal Feature 3 Image',
+    },
+    feature2ImgAlt: {
+      type: String,
+      default: 'Smart Healthcare Portal Feature 2 Image',
+    },
+    feature1Title: {
+      type: String,
+      default: 'Centralized Health Information',
+    },
+    feature1Description: {
+      type: String,
+      default:
+        'Store and manage all health-related information in one secure location',
+    },
+    feature2ImgSrc: {
+      type: String,
+      default:
+        'https://images.unsplash.com/photo-1666886573199-88a2ed65470d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w5MTMyMXwwfDF8cmFuZG9tfHx8fHx8fHx8MTczMzUwMDE2M3w&ixlib=rb-4.0.3&q=80&w=1080',
+    },
+  },
+  data() {
+    return {
+      activeTab: 0,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.features1-container2 {
+  width: 100%;
+  display: grid;
+  grid-gap: var(--dl-space-space-fiveunits);
+  position: relative;
+  grid-template-columns: 1fr 1fr;
+}
+.features1-image-container {
+  height: 100%;
+  display: flex;
+  position: relative;
+}
+.features1-image1 {
+  animation-name: fadeIn;
+  animation-delay: 0s;
+  animation-duration: 300ms;
+  animation-direction: normal;
+  animation-iteration-count: 1;
+  animation-timing-function: ease;
+}
+.features1-image2 {
+  animation-name: fadeIn;
+  animation-delay: 0s;
+  animation-duration: 300ms;
+  animation-direction: normal;
+  animation-iteration-count: 1;
+  animation-timing-function: ease;
+}
+.features1-image3 {
+  animation-name: fadeIn;
+  animation-delay: 0s;
+  animation-duration: 300ms;
+  animation-direction: normal;
+  animation-iteration-count: 1;
+  animation-timing-function: ease;
+}
+.features1-tabs-menu {
+  gap: var(--dl-space-space-twounits);
+  width: 100%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: center;
+}
+.features1-tab-horizontal1 {
+  gap: var(--dl-space-space-twounits);
+  cursor: pointer;
+  display: flex;
+  overflow: hidden;
+  align-self: stretch;
+  align-items: flex-start;
+  flex-shrink: 0;
+}
+.features1-divider-container1 {
+  display: flex;
+  align-self: stretch;
+  align-items: flex-start;
+}
+.features1-container3 {
+  width: 2px;
+  align-self: stretch;
+  background-color: var(--dl-color-theme-neutral-dark);
+}
+.features1-content1 {
+  gap: 16px;
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+  flex-grow: 1;
+  align-items: flex-start;
+  flex-shrink: 0;
+  flex-direction: column;
+  justify-content: center;
+}
+.features1-tab-horizontal2 {
+  gap: var(--dl-space-space-twounits);
+  cursor: pointer;
+  display: flex;
+  overflow: hidden;
+  align-self: stretch;
+  align-items: flex-start;
+  flex-shrink: 0;
+}
+.features1-divider-container2 {
+  display: flex;
+  align-self: stretch;
+  align-items: flex-start;
+}
+.features1-container4 {
+  width: 2px;
+  align-self: stretch;
+  background-color: var(--dl-color-theme-neutral-dark);
+}
+.features1-content2 {
+  gap: 16px;
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+  flex-grow: 1;
+  align-items: flex-start;
+  flex-shrink: 0;
+  flex-direction: column;
+  justify-content: center;
+}
+.features1-tab-horizontal3 {
+  gap: var(--dl-space-space-twounits);
+  cursor: pointer;
+  display: flex;
+  overflow: hidden;
+  align-self: stretch;
+  align-items: flex-start;
+  flex-shrink: 0;
+}
+.features1-divider-container3 {
+  display: flex;
+  align-self: stretch;
+  align-items: flex-start;
+}
+.features1-container5 {
+  width: 2px;
+  align-self: stretch;
+  background-color: var(--dl-color-theme-neutral-dark);
+}
+.features1-content3 {
+  gap: 16px;
+  flex: 1;
+  display: flex;
+  overflow: hidden;
+  flex-grow: 1;
+  align-items: flex-start;
+  flex-shrink: 0;
+  flex-direction: column;
+  justify-content: center;
+}
+@media(max-width: 991px) {
+  .features1-container2 {
+    grid-gap: var(--dl-space-space-twounits);
+    grid-template-columns: 1fr;
+  }
+}
+</style>
---
+++ spotless hungry crocodile-vue/src/views/home.vue
@@ -0,0 +1,104 @@
+<template>
+  <div class="home-container">
+    <app-navbar></app-navbar>
+    <app-banner2>
+      <template v-slot:action1>
+        <div class="home-fragment1">
+          <span class="home-text1">Get Started</span>
+        </div>
+      </template>
+      <template v-slot:content1>
+        <div class="home-fragment2">
+          <span class="home-text2">
+            Your one-stop solution for all your healthcare needs
+          </span>
+        </div>
+      </template>
+      <template v-slot:heading1>
+        <div class="home-fragment3">
+          <span class="home-text3">Welcome to Smart Healthcare Portal</span>
+        </div>
+      </template>
+    </app-banner2>
+    <app-hero></app-hero>
+    <span>
+      <DateTimePrimitive
+        format="DD/MM/YYYY"
+        date="Fri Dec 06 2024 20:50:28 GMT+0500 (Pakistan Standard Time)"
+      ></DateTimePrimitive>
+    </span>
+    <app-features1></app-features1>
+    <app-cta></app-cta>
+    <app-features2></app-features2>
+    <app-pricing></app-pricing>
+    <app-steps></app-steps>
+    <app-testimonial></app-testimonial>
+    <app-contact></app-contact>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import { DateTimePrimitive } from '@teleporthq/react-components'
+
+import AppNavbar from '../components/navbar'
+import AppBanner2 from '../components/banner2'
+import AppHero from '../components/hero'
+import AppFeatures1 from '../components/features1'
+import AppCta from '../components/cta'
+import AppFeatures2 from '../components/features2'
+import AppPricing from '../components/pricing'
+import AppSteps from '../components/steps'
+import AppTestimonial from '../components/testimonial'
+import AppContact from '../components/contact'
+import AppFooter from '../components/footer'
+
+export default {
+  name: 'Home',
+  components: {
+    AppNavbar,
+    AppBanner2,
+    AppHero,
+    DateTimePrimitive,
+    AppFeatures1,
+    AppCta,
+    AppFeatures2,
+    AppPricing,
+    AppSteps,
+    AppTestimonial,
+    AppContact,
+    AppFooter,
+  },
+  metaInfo: {
+    title: 'Spotless Hungry Crocodile',
+  },
+}
+</script>
+
+<style scoped>
+.home-container {
+  width: 100%;
+  display: flex;
+  min-height: 100vh;
+  align-items: center;
+  flex-direction: column;
+}
+.home-fragment1 {
+  display: contents;
+}
+.home-text1 {
+  display: inline-block;
+}
+.home-fragment2 {
+  display: contents;
+}
+.home-text2 {
+  display: inline-block;
+}
+.home-fragment3 {
+  display: contents;
+}
+.home-text3 {
+  display: inline-block;
+}
+</style>
